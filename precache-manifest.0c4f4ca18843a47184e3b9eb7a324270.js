9409453301dc0a882bafd62c66de89aa0d3d93b5
MatildaAg <68851803+MatildaAg@users.noreply.github.com>
2020-08-11 12:30:23 +0000

    first commit
    Week 3 assignment on using React to clone a website

---
+++ precache-manifest.0c4f4ca18843a47184e3b9eb7a324270.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddff7986c0415d1d67cfebbec39898f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b149e62887e5872593e1",
+    "url": "/static/css/main.4c26b541.chunk.css"
+  },
+  {
+    "revision": "d8ea47511e81bbb899ff",
+    "url": "/static/js/2.6512041f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6512041f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b149e62887e5872593e1",
+    "url": "/static/js/main.09dabb23.chunk.js"
+  },
+  {
+    "revision": "a3e73a6212384d8dc39b",
+    "url": "/static/js/runtime-main.e7e56eb7.js"
+  },
+  {
+    "revision": "5f1cd582c6d4fb5f4d86e6c1635deec2",
+    "url": "/static/media/hulk-1.5f1cd582.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "294dd39b7ab19aedb004eb8f359c307f",
+    "url": "/static/media/venom-1.294dd39b.jpg"
+  },
+  {
+    "revision": "5eb62fe8d11e8a32b2fb2554088f308e",
+    "url": "/static/media/venom-2.5eb62fe8.jpg"
+  }
+]);
